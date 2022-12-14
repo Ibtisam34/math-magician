3cc1ac2a27cd65df407e346fc4dd9d89d88ae77d
unknown <Riyanagwl@gmail.com>
2022-12-14 18:09:47 +0300

    modified:   src/logic/operate.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,52 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handler = this.handler.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  handler(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
+  const handler = (e) => {
+    setState((state) => calculate(state, e.target.textContent));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator">
-        <div className="display">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div className="button-panel">
-          <button type="button" onClick={this.handler} className="orange">AC</button>
-          <button type="button" onClick={this.handler} className="orange">+/-</button>
-          <button type="button" onClick={this.handler} className="black">%</button>
-          <button type="button" onClick={this.handler} className="orange">1</button>
-          <button type="button" onClick={this.handler} className="orange">6</button>
-          <button type="button" onClick={this.handler} className="black">+</button>
-          <button type="button" onClick={this.handler} className="orange">2</button>
-          <button type="button" onClick={this.handler} className="orange">7</button>
-          <button type="button" onClick={this.handler} className="black">-</button>
-          <button type="button" onClick={this.handler} className="orange">3</button>
-          <button type="button" onClick={this.handler} className="orange">8</button>
-          <button type="button" onClick={this.handler} className="black">x</button>
-          <button type="button" onClick={this.handler} className="orange">4</button>
-          <button type="button" onClick={this.handler} className="orange">9</button>
-          <button type="button" onClick={this.handler} className="black">÷</button>
-          <button type="button" onClick={this.handler} className="orange">5</button>
-          <button type="button" onClick={this.handler} className="orange" id="zero">0</button>
-          <button type="button" onClick={this.handler} className="black">.</button>
-          <button type="button" onClick={this.handler} className="grey">=</button>
-        </div>
+  return (
+    <div className="calculator">
+      <div className="display">
+        {state.total}
+        {state.operation}
+        {state.next}
       </div>
-    );
-  }
-}
+      <div className="button-panel">
+        <button type="button" onClick={handler} className="orange">AC</button>
+        <button type="button" onClick={handler} className="orange">+/-</button>
+        <button type="button" onClick={handler} className="black">%</button>
+        <button type="button" onClick={handler} className="orange">1</button>
+        <button type="button" onClick={handler} className="orange">6</button>
+        <button type="button" onClick={handler} className="black">+</button>
+        <button type="button" onClick={handler} className="orange">2</button>
+        <button type="button" onClick={handler} className="orange">7</button>
+        <button type="button" onClick={handler} className="black">-</button>
+        <button type="button" onClick={handler} className="orange">3</button>
+        <button type="button" onClick={handler} className="orange">8</button>
+        <button type="button" onClick={handler} className="black">x</button>
+        <button type="button" onClick={handler} className="orange">4</button>
+        <button type="button" onClick={handler} className="orange">9</button>
+        <button type="button" onClick={handler} className="black">÷</button>
+        <button type="button" onClick={handler} className="orange">5</button>
+        <button type="button" onClick={handler} className="orange" id="zero">0</button>
+        <button type="button" onClick={handler} className="black">.</button>
+        <button type="button" onClick={handler} className="grey">=</button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
