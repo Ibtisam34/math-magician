842e955612471657577a2c34d0f7c40768928ccd
pius ssekweyama <ssekweyamapius@gmail.com>
2022-12-22 13:03:41 +0300

    test

--- src/_test_/calculator.test.js
+++ src/_test_/calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Calculator from '../Components/Calculator';
+import Calculator from '../components/calculator';
 
 it('renders correctly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
