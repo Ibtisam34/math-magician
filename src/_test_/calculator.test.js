a554686b94ced523f8f264781eb5fa287f0afe4b
unknown <Riyanagwl@gmail.com>
2022-12-22 12:40:02 +0300

    update calculator

--- src/_test_/calculator.test.js
+++ src/_test_/calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Calculator from '../Components/Calculator';
+import Calculator from '../Components/Calculator.js';
 
 it('renders correctly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
